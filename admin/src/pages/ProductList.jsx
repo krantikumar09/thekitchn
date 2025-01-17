ae47d2fc9fdb464366aabe28c1881d4f0c7a98fc
Krantikumar Mathapati <krantikumar3446@gmail.com>
2025-01-17 17:46:44 +0530

    feat: list all products in admin

--- admin/src/pages/ProductList.jsx
+++ admin/src/pages/ProductList.jsx
@@ -1,7 +1,90 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { API_URL } from "../App";
+import { toast } from "react-toastify";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { X } from "lucide-react";
 
 const ProductList = () => {
-  return <div>ProductList</div>;
+  const [list, setList] = useState([]);
+
+  const fetchList = async () => {
+    const res = await axios.get(`${API_URL}/api/food/list`);
+    console.log(res.data);
+    if (res.data.success) {
+      setList(res.data.data);
+    } else {
+      toast.error("Something went wrong!");
+    }
+  };
+
+  const removeFood = async (foodId) => {
+    const res = await axios.post(`${API_URL}/api/food/remove`, { id: foodId });
+    await fetchList();
+
+    if (res.data.success) {
+      toast.success(res.data.message);
+    } else {
+      toast.error(res.data.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchList();
+  }, []);
+
+  return (
+    <div className="list">
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-28">Image</TableHead>
+            <TableHead>Name</TableHead>
+            <TableHead className="text-center">Category</TableHead>
+            <TableHead className="text-center">Price</TableHead>
+            <TableHead className="text-center">Action</TableHead>
+          </TableRow>
+        </TableHeader>
+
+        <TableBody>
+          {list.map((item, index) => {
+            return (
+              <TableRow key={index}>
+                <TableCell>
+                  <img
+                    className="max-w-20 w-full h-auto object-cover rounded-md"
+                    src={`${API_URL}/images/` + item.image}
+                    alt={item.name}
+                    loading="lazy"
+                  />
+                </TableCell>
+                <TableCell className="font-bold text-textColor-heading">
+                  {item.name}
+                </TableCell>
+                <TableCell className="text-center">{item.category}</TableCell>
+                <TableCell className="text-center">
+                  &#8377; {item.price}
+                </TableCell>
+                <TableCell className="text-center">
+                  <X
+                    onClick={() => removeFood(item._id)}
+                    className="text-center text-xs cursor-pointer"
+                  />
+                </TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+    </div>
+  );
 };
 
 export default ProductList;
