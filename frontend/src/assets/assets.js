10f5521a6fd88b3217682292916e053dcb080cd5
Krantikumar Mathapati <krantikumar3446@gmail.com>
2025-01-27 22:50:05 +0530

    delete unwanted images

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,5 +1,4 @@
-import basket_icon from "./basket_icon.png";
-import logo from "./logo.png";
+import logo from "./the_kitchn_logo.png";
 import avatar from "./avatar.jpg";
 import header_img from "./header-img.jpg";
 import search_icon from "./search_icon.png";
@@ -45,53 +44,23 @@ import food_30 from "./food_30.png";
 import food_31 from "./food_31.png";
 import food_32 from "./food_32.png";
 
-import add_icon_white from "./add_icon_white.png";
-import add_icon_green from "./add_icon_green.png";
 import remove_icon_red from "./remove_icon_red.png";
-import app_store from "./app_store.png";
-import play_store from "./play_store.png";
-import linkedin_icon from "./linkedin_icon.png";
-import facebook_icon from "./facebook_icon.png";
-import twitter_icon from "./twitter_icon.png";
-import cross_icon from "./cross_icon.png";
-import selector_icon from "./selector_icon.png";
-import rating_starts from "./rating_starts.png";
-import profile_icon from "./profile_icon.png";
-import bag_icon from "./bag_icon.png";
-import logout_icon from "./logout_icon.png";
-import parcel_icon from "./parcel_icon.png";
-import exit from "./exit.png";
-import shopping_bag from "./shopping-bag.png";
+import rating_starts from "./rating_starts.png"
 import order_success from "./order_success.gif";
 import order_cancel from "./order_cancel.gif";
 import delivery_man from "./delivery_man.png"
 
+
 export const assets = {
   avatar,
   logo,
-  basket_icon,
   header_img,
   search_icon,
-  rating_starts,
-  add_icon_green,
-  add_icon_white,
   remove_icon_red,
-  app_store,
-  play_store,
-  linkedin_icon,
-  facebook_icon,
-  twitter_icon,
-  cross_icon,
-  selector_icon,
-  profile_icon,
-  logout_icon,
-  bag_icon,
-  parcel_icon,
-  exit,
-  shopping_bag,
   order_success,
   order_cancel,
-  delivery_man
+  delivery_man,
+  rating_starts
 };
 
 export const menu_list = [
